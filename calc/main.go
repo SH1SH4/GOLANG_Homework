befbe41491daa1775c1e3cf8f2f280541d461ed1
sh1sh4 <sh1sh4@vk.com>
2025-03-05 16:46:33 +0300

    Забыл поменять мейн после того как дебажил

--- calc/main.go
+++ calc/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	calculator "calc/Calculator"
 	"fmt"
-	// "os"
+	"os"
 )
 
 func main() {
 	var line string
 	fmt.Print("Введите пример без пробелов :")
-	// fmt.Fscan(os.Stdin, &line)
-	line = "2+5*(2+10*5)"
+	fmt.Fscan(os.Stdin, &line)
+	// line = "2+5*(2+10*5)"
 	answer, err := calculator.Calculate(line)
 	if err == nil {
 		fmt.Printf("%v=%v", line, answer)
